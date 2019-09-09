ad4d627f1d3ff177f16ccebfec460e80df45c0df
Vahram Manukyan <manukyanv07@gmail.com>
2019-09-09 13:34:17 -0300

    Initial commit of modules repo

---
+++ services/webserver-cluster/vars.tf
@@ -0,0 +1,11 @@
+variable "cluster_name" {
+  description = "The name to use for all the cluster resources"
+}
+
+variable "db_remote_state_bucket" {
+  description = "The name of the S3 bucket for the database's remote state"
+}
+
+variable "db_remote_state_key" {
+  description = "The path for the database's remote state in S3"
+}
